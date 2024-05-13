4df54fd6a64e72dca9712c331f130037f3f0e84e
Supakit W <northy@sfs-wifi-dhcp-10-143-26-208.wifi.sfsu.edu>
2024-05-13 16:39:14 -0700

    add vecadd

---
+++ vecadd_cpu.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+      y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<29; // 1M elements
+
+  float *x = new float[N];
+  float *y = new float[N];
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+    // Start the timer
+    auto start = std::chrono::high_resolution_clock::now();
+
+  // Run kernel on 1M elements on the CPU
+  add(N, x, y);
+
+    // Stop the timer
+    auto end = std::chrono::high_resolution_clock::now();
+
+// Calculate the elapsed time
+std::chrono::duration<double> elapsed_seconds = end - start;
+
+// Print the elapsed time
+std::cout << "Elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  delete [] x;
+  delete [] y;
+
+  return 0;
+}
